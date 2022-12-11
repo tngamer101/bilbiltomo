d0114cda2254271f07412de2fbeb49d372f737d1
tngamer101 <103201714+tngamer101@users.noreply.github.com>
2022-12-11 11:49:24 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
-module.exports = {mongoLink:'mongodb+srv://tnvillanyt:suriyaskp@cluster0.plrmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database linkbot_token:'5394744068:AAHekszQY3AhkB-jG6-u-hlbaEq2SIXaliE',bot_name:'CRYPTO_VERSE_AIRDROPBOT',//bot username without @bot_admin:1996486622,channelsList : ['@OfficialEarthQuake','@Crypto_verse1','@Airdropspanther','@PakAirdropsAler','@ATTITUDEARNERS','@AirdropRankers','@TNGairdrop','@airdrop_raja'],channelscheck :['@OfficialEarthQuake','@Crypto_verse1','@Airdropspanther','@PakAirdropsAler','@ATTITUDEARNERS','@AirdropRankers','@TNGairdrop','@airdrop_raja'],airdropName : 'crypto verse.',reffer_bonus:2000000,// ral bonus amountmin_wd:4000000,//min withdrawal amountcurrency:'Crypto Verse', //bot cuurencypayment_channel:'@payoutworldPpp'// payment channel username}
+module.exports = {
+
+mongoLink:'mongodb+srv://tnvillanyt:suriyaskp@cluster0.plrmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5394744068:AAHekszQY3AhkB-jG6-u-hlbaEq2SIXaliE',
+
+bot_name:'CRYPTO_VERSE_AIRDROPBOT',//bot username without @
+
+bot_admin:1774613316,
+
+channelsList : ['@OfficialEarthQuake','@Crypto_verse1','@Airdropspanther','@PakAirdropsAler','@ATTITUDEARNERS','@AirdropRankers','@TNGairdrop','@airdrop_raja'],
+
+channelscheck :['@OfficialEarthQuake','@Crypto_verse1','@Airdropspanther','@PakAirdropsAler','@ATTITUDEARNERS','@AirdropRankers','@TNGairdrop','@airdrop_raja'],
+
+airdropName : 'crypto verse.',
+
+reffer_bonus:2000000,// ral bonus amount
+
+min_wd:4000000,//min withdrawal amount
+
+currency:'Crypto Verse', //bot cuurency
+
+payment_channel:'@payoutworldPpp'// payment channel username
+
+}
